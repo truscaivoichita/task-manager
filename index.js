4245de16323a5ee23135f0cddd4cee6706cc9cfa
truscaivoichita <sabo_voichita4sacrifice@yahoo.com>
2026-03-11 21:22:46 +0100

    add task creation from form input

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 console.log("Inside the app");
+const form = document.getElementById("taskForm");
+const input = document.getElementById("taskInput");
+const list = document.getElementById("taskList");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const taskText = input.value;
+
+  const li = document.createElement("li");
+  li.textContent = taskText;
+
+  list.appendChild(li);
+
+  input.value = "";
+});
